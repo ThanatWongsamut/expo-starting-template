66f5c916c90430a3d998e4f13d1b881b72b83223
ThanatWongsamut <thanat.wongsamut@gmail.com>
2023-11-19 12:37:29 +0700

    [feat] Demo page

--- pages/HomeScreen.tsx
+++ pages/HomeScreen.tsx
@@ -1,10 +1,38 @@
-import { FC } from 'react';
+import { FC, useRef } from 'react';
 import { View, Text } from 'react-native';
 
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+import LottieView from 'lottie-react-native';
+
 const HomeScreen: FC = () => {
+  const animation = useRef(null);
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+    <View className="w-full flex py-4 items-center justify-center bg-white">
+      <View className="flex">
+        <Text>Home Screen</Text>
+      </View>
+
+      <View className="w-full flex items-center justify-center bg-red-300">
+        <Text>Icon</Text>
+        <Ionicons name="md-checkmark-circle" size={32} color="green" />
+      </View>
+
+      <View className="w-full flex items-center justify-center">
+        <Text>Lottie</Text>
+
+        <LottieView
+          autoPlay
+          ref={animation}
+          style={{
+            width: 200,
+            height: 200,
+            backgroundColor: '#eee',
+          }}
+          source={require('@assets/lottie/SampleLoading.json')}
+        />
+      </View>
     </View>
   );
 };
